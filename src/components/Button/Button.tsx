da4673cf7858e1b024b912d8a10358811c08ab6e
Filip Lizoń <filip.lizon1@gmail.com>
2023-05-10 16:08:24 +0200

    style: button

---
+++ src/components/Button/Button.tsx
@@ -0,0 +1,11 @@
+import { button } from "@/utils/styles";
+
+const Button = () => {
+  return (
+    <button className={button} type="submit">
+      Submit
+    </button>
+  );
+};
+
+export default Button;
---
+++ src/utils/styles.ts
@@ -0,0 +1,1 @@
+export const button = "bg-gray-800 w-full py-3 rounded-md text-white";
